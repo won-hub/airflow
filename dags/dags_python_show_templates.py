1fce9b996194088328f275eb1ca0f4d3d8fe8a98
due7670 <moxnox08@daum.net>
2024-04-11 14:27:09 +0900

    show_templates 수정

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 with DAG(
     dag_id="dags_python_show_templates",
     schedule="30 9 * * *",
-    start_date=pendulum.datetime(2023, 3, 10, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 4, 10, tz="Asia/Seoul"),
     catchup=True
 ) as dag:
     

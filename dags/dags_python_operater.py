6953d1389dc1b13edbe64581f0d0bf431d4269f7
due7670 <moxnox08@daum.net>
2024-04-11 10:36:46 +0900

    operater 업데이트2

--- dags/dags_python_operater.py
+++ dags/dags_python_operater.py
@@ -15,9 +15,9 @@ with DAG(
         rand_int = random.randint(0,3)
         print(fruit[rand_int])
 
-        py_t1 = PythonOperator(
-            task_id="py_t1",
-            python_callable=select_fruit
-        )
+    py_t1 = PythonOperator(
+        task_id="py_t1",
+        python_callable=select_fruit
+    )
 
-        py_t1
+    py_t1

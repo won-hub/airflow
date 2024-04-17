c031edcee4d197e620a4165aa585fe59abe95d0f
due7670 <moxnox08@daum.net>
2024-04-17 11:29:46 +0900

    강의 자료 업데이트

--- dags/dags_bash_sensor.py
+++ dags/dags_bash_sensor.py
@@ -3,6 +3,19 @@ from airflow.operators.bash import BashOperator
 from airflow import DAG
 import pendulum
 
+#sensor의 개념 : 
+#1.일종의 특화된 오퍼레이터
+#2.특정 조건이 만족되기를 기다리고 만족되면 True를 반환하는 Task
+#3.모든 센서는 BaseSensorOperator를 상속하여 구현되며(BaseSensorOperator는 BaseOperator를 상속함)
+#  상속시에는 __init()__ 함수와 poke(context) 함수 재정의 필요
+#4.센싱하는 로직은 poke 함수에 정의
+
+#https://airflow.apache.org/docs/apache-airflow/stable/_api/airflow/sensors/base/index.html
+
+#Pool : 
+#모든 Task는 특정 Pool에서 수행되며 Pool은 Slot이라는 것을 가지고 있음.
+#기본적으로 Task 1개당 Slot 1개를 점유하며 Pool을 지정하지 않으면 default_pool에서 수행
+
 with DAG(
     dag_id='dags_bash_sensor',
     start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
